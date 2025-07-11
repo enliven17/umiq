89ec0a87ab085730e5e8c944cbbb3d2b882de502
enliven <cankat.polat@metu.edu.tr>
2025-07-11 16:13:14 +0300

    vercel last

--- src/theme/styled.d.ts
+++ src/theme/styled.d.ts
@@ -1,11 +1,9 @@
+/* eslint-disable @typescript-eslint/no-empty-interface */
 import 'styled-components';
 import { theme } from '.';
 
 type Theme = typeof theme;
 
 declare module 'styled-components' {
-  // eslint-disable-next-line @typescript-eslint/no-empty-interface
-  export interface DefaultTheme extends Theme {
-    // This comment is here to prevent the interface from being considered empty by the linter.
-  }
+  export interface DefaultTheme extends Theme {}
 } 
