dc41682dca99d91274d7a454a83d697d81fdb22d
enliven <cankat.polat@metu.edu.tr>
2025-07-11 15:51:51 +0300

    vercel update 5

--- src/theme/styled.d.ts
+++ src/theme/styled.d.ts
@@ -1,10 +1,2 @@
-import 'styled-components';
 import { theme } from '.';
-
-type Theme = typeof theme;
-
-declare module 'styled-components' {
-  // DefaultTheme extends Theme, linter için açıklama
-  // eslint-disable-next-line @typescript-eslint/no-empty-interface
-  export interface DefaultTheme extends Theme {}
-} 
+export type DefaultTheme = typeof theme; 
