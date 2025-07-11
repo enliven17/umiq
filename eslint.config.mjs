4d50892c5f183c8af7630f8ca3e100496e4f794a
enliven <cankat.polat@metu.edu.tr>
2025-07-11 16:41:11 +0300

    >::::

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -12,6 +12,7 @@ const compat = new FlatCompat({
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
   {
+    ignores: ["src/theme/styled.d.ts"],
     rules: {
       "@typescript-eslint/no-empty-interface": "off",
     },
