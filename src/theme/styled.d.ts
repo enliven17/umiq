4ae42fb5994282ef3c726ff8a025f9bcbe4e3b16
enliven <cankat.polat@metu.edu.tr>
2025-07-11 16:45:31 +0300

    Revert "vercel update 9 last I hope"
    This reverts commit 311b08685e0066abe63e37f2d30b63b0b0dfc6d9.

--- src/theme/styled.d.ts
+++ src/theme/styled.d.ts
@@ -4,8 +4,5 @@ import { theme } from '.';
 type Theme = typeof theme;
 
 declare module 'styled-components' {
-  // eslint-disable-next-line @typescript-eslint/no-empty-interface
-  export interface DefaultTheme extends Theme {
-    // This comment is here to prevent the interface from being considered empty by the linter.
-  }
+  export interface DefaultTheme extends Theme {}
 } 
