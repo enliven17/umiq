c76bb42843b4fcecb792a82f742af873213c2fd5
enliven <cankat.polat@metu.edu.tr>
2025-07-11 16:45:07 +0300

    Revert "vercel last"
    This reverts commit 89ec0a87ab085730e5e8c944cbbb3d2b882de502.

--- src/theme/styled.d.ts
+++ src/theme/styled.d.ts
@@ -1,9 +1,11 @@
-/* eslint-disable @typescript-eslint/no-empty-interface */
 import 'styled-components';
 import { theme } from '.';
 
 type Theme = typeof theme;
 
 declare module 'styled-components' {
-  export interface DefaultTheme extends Theme {}
+  // eslint-disable-next-line @typescript-eslint/no-empty-interface
+  export interface DefaultTheme extends Theme {
+    // This comment is here to prevent the interface from being considered empty by the linter.
+  }
 } 
